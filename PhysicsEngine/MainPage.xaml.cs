980c80f9755ee7f83570cef928426eb4300226d9
JacobMabey <mabey.jacob@gmail.com>
2023-10-03 18:18:43 -0600

    Setup Main Scene

--- PhysicsEngine/MainPage.xaml.cs
+++ PhysicsEngine/MainPage.xaml.cs
@@ -5,13 +5,16 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +28,17 @@ namespace PhysicsEngine
         public MainPage()
         {
             this.InitializeComponent();
+
+            int x = 50;
+            Ellipse circ = new Ellipse();
+            circ.Height = x += 10;
+            circ.Width = 100;
+            SolidColorBrush red = new SolidColorBrush();
+            red.Color = Colors.Red;
+            circ.Fill = red;
+            Canvas.SetLeft(circ, 100);
+            Canvas.SetTop(circ, 200);
+            MainScene.Children.Add(circ);
         }
     }
 }
