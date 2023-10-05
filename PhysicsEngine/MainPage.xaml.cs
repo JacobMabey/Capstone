d7e2f5565c2fd64872c53e32d34f21bedb19093f
JacobMabey <mabey.jacob@gmail.com>
2023-10-04 20:40:48 -0500

    Created a few components and fully setup game environment

--- PhysicsEngine/MainPage.xaml.cs
+++ PhysicsEngine/MainPage.xaml.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Xml.Linq;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
@@ -25,20 +27,85 @@ namespace PhysicsEngine
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        //Main Global Canvas
+        public static Canvas MainScene { get; private set; }
+
+
+
+        int x = 50;
+        Ellipse circ = new Ellipse();
+        CompRectangle rect;
+        Particle p;
+
         public MainPage()
         {
-            this.InitializeComponent();
+            Initialize(Colors.White); //Main Initial Canvas Initialization
 
-            int x = 50;
-            Ellipse circ = new Ellipse();
-            circ.Height = x += 10;
+
+            circ.Height = 100;
             circ.Width = 100;
             SolidColorBrush red = new SolidColorBrush();
             red.Color = Colors.Red;
             circ.Fill = red;
-            Canvas.SetLeft(circ, 100);
+            Canvas.SetLeft(circ, x);
             Canvas.SetTop(circ, 200);
             MainScene.Children.Add(circ);
+
+            rect = new CompRectangle(new Point(300, 50), new Point(50, 80));
+            rect.RotationCenter = new Point(100, 100);
+            MainScene.Children.Add(rect.UIRect);
+
+            //p = new Particle(new Point(500, 500), 10);
+            //MainScene.Children.Add(p.UIEllipse);
+        }
+
+        private void Loop(object sender, object e) 
+        {
+            Update(); //Main Canvas Update To be within Loop
+
+
+            double left = Canvas.GetLeft(circ);
+            Canvas.SetLeft(circ, left + 1 * Timer.DeltaTime);
+
+
+            rect.RotationAngle = (rect.RotationAngle + 1 * Timer.DeltaTime) % 360.0;
+            if (rect.RotationAngle > 180)
+                rect.Fill = Colors.Red;
+            else
+                rect.Fill = Colors.Blue;
+
+            if (left > 1200)
+                Renderer.SetBgColor(Colors.Gray);
+        }
+
+
+
+        private void Initialize(Color bgColor)
+        {
+            //Add Initial Windows/Canvases
+            this.InitializeComponent();
+
+            MainScene = new Canvas();
+            MainScene.Width = MainCanvas.Width;
+            MainScene.Height = MainCanvas.Height;
+            MainCanvas.Children.Add(MainScene);
+            //
+
+            //Initialize Systems
+            CompositionTarget.Rendering += Loop;
+
+            Timer.Initialize();
+            Renderer.Initialize(bgColor);
+            //
+        }
+
+
+        private void Update()
+        {
+            //Update Systems
+            Timer.Update();
+            Renderer.Update();
+            //
         }
     }
 }
